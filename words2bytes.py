e68776dd7a67ef59847088d6cbecbe17b51f217b
Sam Gbafa <skgbafa@gmail.com>
2021-02-16 02:04:21 +0000

    cleanup

--- words2bytes.py
+++ words2bytes.py
@@ -1,4 +1,3 @@
-
 # imports
 import time
 import math
@@ -8,27 +7,45 @@ import torch.nn as nn
 
 import wandb
 
-from transformer import DecoderOnlyTransformer
 from constants import *
+from artifacts import initalize_artifacts, visualize_artifacts
+from utils import extract_config
+
+from data import load_data, batchify
+from transformer import DecoderOnlyTransformer
+from training import train, evaluate
 
 
-
+# default config
+default_config = {
+    "embedding_dimension": 200,
+    "ff_dimension": 200,
+    "n_attention_heads": 2,
+    "n_encoder_layers": 0,
+    "n_decoder_layers": 2,
+    "dataset": Dataset.PennTreebank.name,
+    "segmentation": Segmentation.Word.name,
+    "max_seq_len": 35,
+    "batch_size": 20,
+    "eval_batch_size": 10,
+    "dropout": 0.2,
+    "n_epochs": 3,
+    "learning_rate": 5.0,
+    "loss_criterion": "CrossEntropyLoss"
+}
 
 # experiment generation
 def generateExperiements():
-    WANDB_ENTITY = "skgbafa"
-    # WANDB_ENTITY = "openai-scholars"
-    WANDB_PROJECT = ""
+    # WANDB_ENTITY = "skgbafa"
+    WANDB_ENTITY = "openai-scholars"
 
     # experiment_datasets = [ Dataset.PennTreebank.name, Dataset.WikiText2.name, Dataset.WikiText103.name ]
-    experiment_datasets = [Dataset.WikiText2.name]
-    experiment_segmentation = [
-        Segmentation.Word.name, Segmentation.Character.name]
-    # for each dataset
-    #
+    experiment_datasets = [ Dataset.WikiText2.name ]
+    experiment_segmentation = [ Segmentation.Word.name, Segmentation.Character.name ]
+
     sweep_parameters = {
         "n_attention_heads": {
-            "values": [2, 3 ]
+            "values": [2, 3]
         },
         "n_decoder_layers": {
             "values": [2, 4, 6]
@@ -54,17 +71,10 @@ def generateExperiements():
 
     return sweep_id
 
-
-
-
-
 # sweep function
-
-
 def train_and_eval():
     run = wandb.init(config=default_config)
     config = run.config
-    print(config)
 
     # setup data
     # extract config vars
@@ -86,13 +96,21 @@ def train_and_eval():
 
     # instantiate model
     model = DecoderOnlyTransformer(ntokens, d_model=embedding_dimension, nhead=n_attention_heads, num_encoder_layers=n_encoder_layers,
-                                   num_decoder_layers=n_decoder_layers, dim_feedforward=ff_dimension, dropout=dropout).to(device)
+                                num_decoder_layers=n_decoder_layers, dim_feedforward=ff_dimension, dropout=dropout).to(device)
 
     # hyperparams
-    # lr = 5.0 # learning rate
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+    # runtime vars
+    runtime = {
+        "criterion": criterion,
+        "optimizer": optimizer,
+        "scheduler": scheduler,
+        "ntokens": ntokens,
+        "device": device,
+    }
 
     # train loop
     best_val_loss = float("inf")
@@ -104,14 +122,14 @@ def train_and_eval():
     for epoch in range(1, epochs + 1):
         epoch_start_time = time.time()
 
-        train(model, config, epoch, artifacts)
-        val_loss = evaluate(model, val_data_batches, config)
+        train(model, train_data_batches, config, runtime, epoch, artifacts)
+        val_loss = evaluate(model, val_data_batches, config, runtime)
         wandb.log({"val_loss": val_loss, "val_ppl": math.exp(
             val_loss), "epoch": epoch})
         print('-' * 89)
         print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
-              'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
-                                         val_loss, math.exp(val_loss)))
+            'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
+                                        val_loss, math.exp(val_loss)))
         print('-' * 89)
 
         if val_loss < best_val_loss:
@@ -123,7 +141,7 @@ def train_and_eval():
     visualize_artifacts(artifacts)
 
     # test model
-    test_loss = evaluate(best_model, test_data_batches, config)
+    test_loss = evaluate(best_model, test_data_batches, config, runtime)
     wandb.log({"test_loss": test_loss, "test_ppl": math.exp(test_loss)})
 
     print('=' * 89)
@@ -134,26 +152,11 @@ def train_and_eval():
     return best_model
 
 
-default_config = {
-    "embedding_dimension": 200,
-    "ff_dimension": 200,
-    "n_attention_heads": 2,
-    "n_encoder_layers": 0,
-    "n_decoder_layers": 2,
-    "dataset": Dataset.PennTreebank.name,
-    "segmentation": Segmentation.Character.name,
-    "max_seq_len": 35,
-    "batch_size": 20,
-    "eval_batch_size": 10,
-    "dropout": 0.2,
-    "n_epochs": 3,
-    "learning_rate": 0.5,
-    "loss_criterion": "CrossEntropyLoss"
-}
-
-# train_and_eval()
-
 if __name__ == "__main__":
   print("Run Sweep")
-else:
-  print("Functions to Run Sweep")
+  # set up experiments
+  sweep_id = generateExperiements()
+
+  # run experiments
+  wandb.agent(sweep_id, function=train_and_eval)
+
