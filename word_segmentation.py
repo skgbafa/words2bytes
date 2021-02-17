7ceadbe1d80f80ae2e9c5153564ff13e13a736a4
Sam Gbafa <skgbafa@gmail.com>
2021-02-17 20:11:28 +0000

    run sync

--- word_segmentation.py
+++ word_segmentation.py
@@ -24,10 +24,28 @@ benchmark_config_1 = {
     "embedding_dimension": 768,  # units
     "ff_dimension": 3072,  # hidden_size
     "n_attention_heads": 12,  # num_heads
+    "n_encoder_layers": 0,  # num_layers
     "n_decoder_layers": 12,  # num_layers
     "dataset": Dataset.WikiText2.name,
     "segmentation": Segmentation.Word.name,
     "max_seq_len": 512,  # max_length
+    "dropout": 0.1,  # dropout
+    "batch_size": 8,
+    "eval_batch_size": 8,
+    "n_epochs": 6,
+    "learning_rate": 5.0,
+    "loss_criterion": "CrossEntropyLoss"
+}
+
+benchmark_config_1_small = {
+    "embedding_dimension": 384,  # units
+    "ff_dimension": 1536,  # hidden_size
+    "n_attention_heads": 6,  # num_heads
+    "n_encoder_layers": 0,  # num_layers
+    "n_decoder_layers": 6,  # num_layers
+    "dataset": Dataset.WikiText2.name,
+    "segmentation": Segmentation.Word.name,
+    "max_seq_len": 256,  # max_length
     "dropout": 0.1,  # dropout
     "batch_size": 16,
     "eval_batch_size": 8,
